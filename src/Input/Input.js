d7c60b2c5fa638ec092cb3cd097866b501892432
karisikdzemil <karisikdzemil@gmail.com>
2023-01-21 20:59:16 +0100

    bad

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,9 +1,15 @@
 import "./Input.css"
+// import { useState } from "react"
 function Input (){
-
+    // const [vrednost, setVrednost] = useState()
+    const value = (event) =>{
+        const bet = event.target.value
+        return bet
+    }
+    
     return(
         <div className="parent-input">
-            <input placeholder="Enter your to do list" className="child"/>
+            <input onChange={value} placeholder="Enter your to do list" className="child"/>
             <button className="butt">Enter</button>
         </div>
     )
