5740a0deb3edfb044c52f201e8696fd3ee5eeab0
karisikdzemil <karisikdzemil@gmail.com>
2023-02-24 17:46:57 +0100

    tod

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import AddTodo from "./components/AddTodo";
 import TodoItem from "./components/TodoItem";
+import "./style.css"
 import { useState } from "react";
 function App(props) {
-  
+  const [todo, setTodo] = useState([])
+  const getItem = (item) =>{
+    setTodo((prevState)=>{
+     return  [...prevState, item]
+    })
+  }
+  const getCompleted = (id) =>{
+    console.log(id) 
+  }
   return(
-    <div>
-      <AddTodo/>
-      <TodoItem/>
+   <div className="main">
+     <div className="parent">
+    <h1 className="heading">ToDo list</h1>
+      <AddTodo item={getItem}/>
+      {todo && todo.map((todo, index) => (<TodoItem id={index} item={todo} completed={getCompleted}/>))}
+
     </div>
+   </div>
   )
 }
 
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React from "react";
+import "../style.css";
+import { useState } from "react";
 
-export default function AddTodo() {
+export default function AddTodo(props) {
+  const [item, setItem] = useState("");
+  const handleChange = (event) => {
+    setItem(event.target.value)
+  };
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    props.item(item)
+  }
+
   return (
-    <form method='post'>
-      <input type="text" name="item" placeholder='Enter Todo' />
-      <button>Add</button>
-      <div>
-      
-      </div>
+    <form onSubmit={handleSubmit} className="AddTodo" method="post">
+      <input
+        className="AddTodoInput"
+        value={item}
+        onChange={handleChange}
+        type="text"
+        name="item"
+        placeholder="Enter Todo"
+      />
+      <button type="submit" className="AddTodoButt">Add</button>
     </form>
-    )
+  );
 }
